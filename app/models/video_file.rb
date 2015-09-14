a0f39fc9c564f5cf4b9cb04fc512acc50e358f20
Andrew Myers <afredmyers@gmail.com>
2015-09-14 10:44:19 -0400

    Adds an EBUCore metadata field to the VideoFile model.
    Adds some development-enviornment-specific stuff to gitignore.

--- app/models/video_file.rb
+++ app/models/video_file.rb
@@ -4,4 +4,8 @@ class VideoFile < ActiveFedora::Base
   include ::CurationConcerns::GenericWorkBehavior
   include ::CurationConcerns::BasicMetadata
   validates_presence_of :title,  message: 'Your work must have a title.'
+
+  property :video_format, predicate: ::RDF::Vocab::EBUCore.VideoFormat do |index|
+    index.as :stored_searchable, :facetable
+  end
 end
