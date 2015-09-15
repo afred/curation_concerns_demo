c3c40dbeaa58ad84d8e1c8f37bce4c57dc5ad93e
Andrew Myers <afredmyers@gmail.com>
2015-09-15 17:56:35 -0400

    Adds EBUCore VideoFormat as #format.

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,8 @@ class Video < ActiveFedora::Base
   include ::CurationConcerns::GenericWorkBehavior
   include ::CurationConcerns::BasicMetadata
   validates_presence_of :title,  message: 'Your work must have a title.'
+
+  property :format, predicate: ::RDF::Vocab::EBUCore.VideoFormat, multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,7 +3,13 @@
 require 'rails_helper'
 
 describe Video do
-  it "should have some tests" do
-    skip "Add your tests here"
+
+  let(:video) { Video.new }
+
+  describe '#format' do
+    it 'is not multi-valued' do
+      expect{ video.format = "blerg" }.to_not raise_error
+      expect{ video.format = ["blerg"] }.to raise_error
+    end
   end
 end
